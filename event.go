56b63f1937b0de203d40b2a5892d4acea3014a45
agoalofalife <agoalofalife@gmail.com>
2017-07-02 15:58:12 +0300

    structure project

--- event.go
+++ event.go
@@ -1,12 +1,30 @@
 package event
 
+import (
+	"reflect"
+	"fmt"
+)
 
 type Event interface{
 
 }
 
+// display map events
+//     event [name-events] --
+//			   -- [type-structure] *link
+
 type Dispatcher struct {
 	// list listeners
-	listeners map[string]interface{}
+	listenersToFunction map[string] func()
 }
 
+func (dispatcher *Dispatcher) Add(name string, execute interface{}) {
+	fmt.Println(getType(execute))
+	//dispatcher.listenersToFunction[name] = closure
+}
+
+
+// get type  (func(), string ..)
+func getType(some interface{}) reflect.Type {
+	return reflect.TypeOf(some)
+}
