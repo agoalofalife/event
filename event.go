0ce6acb23672fc4baab1f329c582096963c1531e
agoalofalife <agoalofalife@gmail.com>
2017-07-03 15:21:11 +0300

    fix state

--- event.go
+++ event.go
@@ -2,13 +2,11 @@ package event
 
 import (
 	"reflect"
-	"log"
 )
 
-type Event interface{
+type Event interface {
 	Add(name string, performing interface{})
 }
-
 
 type Dispatcher struct {
 	// list listeners
@@ -19,10 +17,12 @@ type Dispatcher struct {
 
 	listeners map[string]map[int]map[string]interface{}
 }
+
 // start
-func Constructor()  *Dispatcher{
+func Constructor() *Dispatcher {
 	d := &Dispatcher{}
-	d.listeners = make(map[string]map[int]map[string]interface{})
+	//d.listeners = make(map[string]map[int]map[string]interface{})
+	d.listeners = map[string]map[int]map[string]interface{}{}
 	return d
 }
 
@@ -31,20 +31,36 @@ func (dispatcher *Dispatcher) Add(name string, performing interface{}) {
 	dispatcher.addListeners(name, performing)
 }
 
-func (dispatcher *Dispatcher) addListeners(name string, performing interface{})  {
+func (dispatcher *Dispatcher) addListeners(name string, performing interface{}) {
 	nameType := getType(performing)
 
-	       if dispatcher.listeners[name] != nil{
-		       dispatcher.listeners[name][len(dispatcher.listeners[name])][nameType.String()] = performing
-	       } else{
-		       dispatcher.listeners = map[string]map[int]map[string]interface{}{
-			       name : map[int]map[string]interface{}{
-				       len(dispatcher.listeners[name]) : map[string]interface{}{
-					       nameType.String() : performing,
-				       },
-			       },
-		       }
-	       }
+	//if dispatcher.listeners[name] != nil{
+	//       dispatcher.listeners[name][len(dispatcher.listeners[name])][nameType.String()] = performing
+	//} else{
+	//       dispatcher.listeners = map[string]map[int]map[string]interface{}{
+	//	       name : map[int]map[string]interface{}{
+	//		       len(dispatcher.listeners[name]) : map[string]interface{}{
+	//			       nameType.String() : performing,
+	//		       },
+	//	       },
+	//       }
+	//}
+
+	//dispatcher.listeners = map[string]map[int]map[string]interface{}{}
+
+	//dispatcher.listeners[name][len(dispatcher.listeners[name])] = map[string]interface{}{
+	//	nameType.String() : performing,
+	//}
+	//dispatcher.listeners[name][len(dispatcher.listeners[name])] = make(map[string]interface{})
+
+	//dispatcher.listeners[name][len(dispatcher.listeners[name])][ nameType.String()] = performing
+
+	if _, exist := dispatcher.listeners[name]; !exist {
+		dispatcher.listeners[name] = map[int]map[string]interface{}{}
+	}
+	dispatcher.listeners[name][len(dispatcher.listeners[name])] = map[string]interface{}{
+		nameType.String(): performing,
+	}
 }
 
 // get type  (func(), string ..)
