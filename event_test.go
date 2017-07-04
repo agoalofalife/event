db1d5bfab81d222d7fee8c79ffd20074cef1b31a
agoalofalife <agoalofalife@gmail.com>
2017-07-04 13:27:40 +0300

    add  file CI

--- event_test.go
+++ event_test.go
@@ -1,1 +1,18 @@
 package event
+
+import (
+	"testing"
+)
+
+func createEvent() *Dispatcher {
+	return Constructor()
+}
+func TestDestroy(t *testing.T) {
+	event := createEvent()
+	event.Add("test", func() {}, []interface{}{})
+	event.Destroy("test")
+
+	if event.existEvent("test") {
+		t.Error("event not deleted")
+	}
+}
