b74ef9c13a7d4052e08fc0fef243a0665888ba1e
agoalofalife <agoalofalife@gmail.com>
2017-07-04 14:51:30 +0300

    add commit

--- event_test.go
+++ event_test.go
@@ -2,11 +2,14 @@ package event
 
 import (
 	"testing"
+	"log"
+	"os"
 )
 
 func createEvent() *Dispatcher {
 	return Constructor()
 }
+
 func TestDestroy(t *testing.T) {
 	event := createEvent()
 	event.Add("test", func() {}, []interface{}{})
@@ -15,4 +18,14 @@ func TestDestroy(t *testing.T) {
 	if event.existEvent("test") {
 		t.Error("event not deleted")
 	}
+}
+
+func TestUntie(t *testing.T)  {
+	event := createEvent()
+	closure := func() {}
+	event.Add("test", closure, []interface{}{})
+	event.Untie(closure)
+	log.Println(event.listeners)
+	os.Exit(2)
+	event.Fire("test")
 }
