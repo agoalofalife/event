0f3a496e8a0155e51c57051f7cff2e020f73e0d0
agoalofalife <agoalofalife@gmail.com>
2017-07-04 15:30:01 +0300

    add test

--- event_test.go
+++ event_test.go
@@ -2,7 +2,10 @@ package event
 
 import (
 	"testing"
+	"reflect"
 )
+
+const nameEvent  = "test"
 
 func createEvent() *Dispatcher {
 	return Constructor()
@@ -10,10 +13,10 @@ func createEvent() *Dispatcher {
 
 func TestDestroy(t *testing.T) {
 	event := createEvent()
-	event.Add("test", func() {}, []interface{}{})
-	event.Destroy("test")
+	event.Add(nameEvent, func() {}, []interface{}{})
+	event.Destroy(nameEvent)
 
-	if event.existEvent("test") {
+	if event.existEvent(nameEvent) {
 		t.Error("event not deleted")
 	}
 }
@@ -21,10 +24,45 @@ func TestDestroy(t *testing.T) {
 func TestUntie(t *testing.T)  {
 	event := createEvent()
 	closure := func() {}
-	event.Add("test", closure, []interface{}{})
+	event.Add(nameEvent, closure, []interface{}{})
 	event.Untie(closure)
 
 	if event.existSubscriber(closure) {
 		t.Error("Subscriber not deleted")
 	}
+}
+
+func TestAdd(t *testing.T)  {
+	event := createEvent()
+	closure := func() {}
+	event.Add(nameEvent, closure, []interface{}{})
+
+	if event.listeners[nameEvent][0][typing] != "func" {
+		t.Error("not installed typing")
+	}
+
+	if reflect.ValueOf(event.listeners[nameEvent][0][structure]).Pointer() != reflect.ValueOf(closure).Pointer() {
+		t.Error("not installed structure")
+	}
+}
+
+// TODO so not work test
+//func TestGo(t *testing.T)  {
+//	event := createEvent()
+//	nameString := "exist"
+//	closure := func(test string) string{
+//		return  test
+//	}
+//	event.Add(nameEvent, closure, []interface{}{nameString})
+//
+//	event.Go(nameEvent)
+//}
+
+func TestGetName(t *testing.T)  {
+	type Test struct {}
+
+	if GetName(Test{}) != "Test"{
+		t.Error("GetName structure error")
+	}
+
 }
